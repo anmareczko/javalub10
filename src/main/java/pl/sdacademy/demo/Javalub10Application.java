16b1d1be3023e45d0e129570e86275492eca6ba4
Bernard Boguszewski <bernard.boguszewski@indexcopernicus.com>
2019-06-02 09:00:37 +0200

    TDD & Pair Programming

---
+++ src/main/java/pl/sdacademy/demo/Javalub10Application.java
@@ -0,0 +1,13 @@
+package pl.sdacademy.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Javalub10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Javalub10Application.class, args);
+	}
+
+}
---
+++ src/test/java/pl/sdacademy/demo/Javalub10ApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.sdacademy.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Javalub10ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
